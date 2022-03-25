1464d9ab7d56ec6453199892a2fc608a4ca62339
mebdog <barak.morris@colorado.edu>
2022-03-25 11:58:55 -0600

    Updated sys of eq to match writeup

--- pr01/code/Main.m
+++ pr01/code/Main.m
@@ -1,23 +1,10 @@
 v0 = 1500; % m/s
 theta = pi/4;
-y0 = Initialize(theta,v0);
+y0 = [0,0,v0,theta]; % x, y, v, theta
 obs = Sensors;
 
-[X,Y] = ode45(@ode_func,0:1550,y0);
+[X,Y] = ode45(@ode_func,0:.0001:30,y0);
 
-largePositiveIndices = (obs(:,1) >= 7) & (obs(:,1) <= 10);
-mediumPositiveIndices = (obs(:,1) >= 3) & (obs(:,1) < 7);
-smallPositiveIndices = (obs(:,1) > 0) & (obs(:,1) < 3);
-smallNegativeIndices = (obs(:,1) < 0) & (obs(:,1) > -3);
-mediumNegativeIndices = (obs(:,1) <= -3) & (obs(:,1) > -7);
-largeNegativeIndices = (obs(:,1) <= -7) & (obs(:,1) >= -10);
-xValues = obs(:,2);
-yValues = obs(:,3);
 hold on
-plot(X,Y(:,4))
-plot(xValues(largePositiveIndices),yValues(largePositiveIndices),'r.','MarkerSize',24)
-plot(xValues(mediumPositiveIndices),yValues(mediumPositiveIndices),'r.','MarkerSize',18)
-plot(xValues(smallPositiveIndices),yValues(smallPositiveIndices),'r.','MarkerSize',12)
-plot(xValues(smallNegativeIndices),yValues(smallNegativeIndices),'b.','MarkerSize',12)
-plot(xValues(mediumNegativeIndices),yValues(mediumNegativeIndices),'b.','MarkerSize',18)
-plot(xValues(largeNegativeIndices),yValues(largeNegativeIndices),'b.','MarkerSize',24)
+plot(obs(:,2),obs(:,3),'.')
+plot(Y(:,1),Y(:,2))
--- pr01/code/ode_func.m
+++ pr01/code/ode_func.m
@@ -1,11 +1,10 @@
-function dy = ode_func(x,y)
+function dy = ode_func(t,y)
     g = 9.81; % m/s^2
-    d = 0.002; % m^-1
-    v = sqrt(y(1)^2+y(2)^2);
+    cd = 0.002; % m^-1
     dy = zeros(4,1);
-    dy(1) = -d*v;
-    dy(2) = (-d*v*y(2)-g)/y(1);
-    dy(3) = 1/y(1);
-    dy(4) = y(2)/y(1);
+    dy(1) = y(3)*cos(y(4)); % x'
+    dy(2) = y(3)*sin(y(4)); % y'
+    dy(3) = -cd*y(3)^2-g*sin(y(4)); %v'
+    dy(4) = -g/y(3)*cos(y(4)); % theta'
 end
 
