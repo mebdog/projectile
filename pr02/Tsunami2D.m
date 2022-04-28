640108577edfcb103b6da8ea5e50d8fad723a0ba
mebdog <barak.morris@colorado.edu>
2022-04-27 20:07:21 -0600

    Edge and Corner Cases- still troubleshooting

--- pr02/Tsunami2D.m
+++ pr02/Tsunami2D.m
@@ -34,7 +34,7 @@ constantCurrent = 2-4*lam*h; %u(i,j)
 kk = [.5,.8,.5,.5];
 ss = [1000,1000,1000];
 surfl(x,y,uCur,ss,kk), xlabel('X axis'), ylabel('Y axis'),
-                 title('Wave'), axis([0 4 0 4 -1 1]),
+                 title('Wave'), axis([0 4 0 4 -1 2]),
                  shading interp
 drawnow
 
@@ -43,44 +43,41 @@ for t = 1:nFrames
         for i = 1:n
             if i == 1
                 uFut(i,j) = leftBoundary(t, deltaT);
-                continue;
-            end
-            if i > 1 && i < n && j > 1 && j < n
-                phantomTop = uCur(i+1,j);
-                phantomLeft = uCur(i,j-1);
-                phantomRight = uCur(i,j+1);
-            else
-                [phantomTop, phantomLeft, phantomRight] = getPhantom(i,j,n,uCur);
-            end
-           
-            uFut(i,j) = constantBottom(i,j)*uCur(i-1,j) + constantTop(i,j)*phantomTop  ...
-                      + constantLeft(i,j)*phantomLeft + constantRight(i,j)*phantomRight ...
-                      + constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
-
-            if i == 2 && j == 1
-                disp([phantomRight, phantomTop, phantomLeft]);
-                disp(uFut(i,j));
-            end
             % Corner Cases
-            %elseif i == n && j == 1
-            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i,j) + uCur(i,j) + uCur(i,j+1)) - uPast(i,j);
-            %elseif i == n && j == n
-            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i,j) + uCur(i,j-1) + uCur(i,j)) - uPast(i,j);
+            elseif i == n && j == 1
+                uFut(i,j) = constantTop(i,j)*uCur(i,j) + constantBottom(i,j)*uCur(i-1,j) + ...
+                            constantLeft(i,j)*uCur(i,j) + constantRight(i,j)*uCur(i,j+1) + ...
+                            constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
+            elseif i == n && j == n
+                uFut(i,j) = constantTop(i,j)*uCur(i,j) + constantBottom(i,j)*uCur(i-1,j) + ...
+                            constantLeft(i,j)*uCur(i,j-1) + constantRight(i,j)*uCur(i,j) + ...
+                            constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
             % Edge Cases
-            %elseif i == n
-            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i,j) + uCur(i,j-1) + uCur(i,j+1)) - uPast(i,j);
-            %elseif j == n
-            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i+1,j) + uCur(i,j-1) + uCur(i,j)) - uPast(i,j);
-            %elseif j == 1
-            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i+1,j) + uCur(i,j) + uCur(i,j+1)) - uPast(i,j);
-            %else
-            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i+1,j) + uCur(i,j-1) + uCur(i,j+1)) - uPast(i,j);
-            %end
+            elseif i == n
+                uFut(i,j) = constantTop(i,j)*uCur(i,j) + constantBottom(i,j)*uCur(i-1,j) + ...
+                            constantLeft(i,j)*uCur(i,j-1) + constantRight(i,j)*uCur(i,j+1) + ...
+                            constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
+            elseif j == 1
+                uFut(i,j) = constantTop(i,j)*uCur(i+1,j) + constantBottom(i,j)*uCur(i-1,j) + ...
+                            constantLeft(i,j)*uCur(i,j) + constantRight(i,j)*uCur(i,j+1) + ...
+                            constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
+            elseif j == n
+                uFut(i,j) = constantTop(i,j)*uCur(i+1,j) + constantBottom(i,j)*uCur(i-1,j) + ...
+                            constantLeft(i,j)*uCur(i,j-1) + constantRight(i,j)*uCur(i,j) + ...
+                            constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
+            else
+                uFut(i,j) = constantTop(i,j)*uCur(i+1,j) + constantBottom(i,j)*uCur(i-1,j) + ...
+                            constantLeft(i,j)*uCur(i,j-1) + constantRight(i,j)*uCur(i,j+1) + ...
+                            constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
+            end
+            if(abs(uFut(i,j)) > 1)
+                uFut(i,j) = 1;
+            end
         end
     end
     
     surfl(x,y,uCur,ss,kk), xlabel('X axis'), ylabel('Y axis'),
-                 title('Wave'), axis([0 4 0 4 -1 1]),
+                 title('Wave'), axis([0 4 0 4 -1 2]),
                  shading interp
     drawnow
     uPast = uCur;
