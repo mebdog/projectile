2f6559c888dcbcfce1274682d01b7643a016802b
mebdog <barak.morris@colorado.edu>
2022-04-27 22:16:20 -0600

    Highlight issue

--- pr02/Tsunami2D.m
+++ pr02/Tsunami2D.m
@@ -8,7 +8,7 @@ set(gcf,'Colormap',AKWaterColormap)
 
 deltaX = .01;
 deltaT = .01;
-c = 1;
+c = .5;
 lam = (c*deltaT/deltaX)^2;
 
 nFrames = 400;
@@ -34,7 +34,7 @@ constantCurrent = 2-4*lam*h; %u(i,j)
 kk = [.5,.8,.5,.5];
 ss = [1000,1000,1000];
 surfl(x,y,uCur,ss,kk), xlabel('X axis'), ylabel('Y axis'),
-                 title('Wave'), axis([0 4 0 4 -1 2]),
+                 title('Wave'), axis([0 4 0 4 0 1]),
                  shading interp
 drawnow
 
@@ -43,41 +43,42 @@ for t = 1:nFrames
         for i = 1:n
             if i == 1
                 uFut(i,j) = leftBoundary(t, deltaT);
+                uFut(i+1,j) = leftBoundary(t, deltaT);
             % Corner Cases
             elseif i == n && j == 1
-                uFut(i,j) = constantTop(i,j)*uCur(i,j) + constantBottom(i,j)*uCur(i-1,j) + ...
-                            constantLeft(i,j)*uCur(i,j) + constantRight(i,j)*uCur(i,j+1) + ...
+                uFut(i,j) = constantTop(i,j)*(2*uCur(i,j)-uCur(i-1,j)) + constantBottom(i,j)*uCur(i-1,j) + ...
+                            constantLeft(i,j)*(2*uCur(i,j)-uCur(i,j+1)) + constantRight(i,j)*uCur(i,j+1) + ...
                             constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
             elseif i == n && j == n
-                uFut(i,j) = constantTop(i,j)*uCur(i,j) + constantBottom(i,j)*uCur(i-1,j) + ...
-                            constantLeft(i,j)*uCur(i,j-1) + constantRight(i,j)*uCur(i,j) + ...
+                uFut(i,j) = constantTop(i,j)*(2*uCur(i,j)-uCur(i-1,j)) + constantBottom(i,j)*uCur(i-1,j) + ...
+                            constantLeft(i,j)*uCur(i,j-1) + constantRight(i,j)*(2*uCur(i,j)-uCur(i,j-1)) + ...
                             constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
             % Edge Cases
             elseif i == n
-                uFut(i,j) = constantTop(i,j)*uCur(i,j) + constantBottom(i,j)*uCur(i-1,j) + ...
+                uFut(i,j) = constantTop(i,j)*(2*uCur(i,j)-uCur(i-1,j)) + constantBottom(i,j)*uCur(i-1,j) + ...
                             constantLeft(i,j)*uCur(i,j-1) + constantRight(i,j)*uCur(i,j+1) + ...
                             constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
             elseif j == 1
                 uFut(i,j) = constantTop(i,j)*uCur(i+1,j) + constantBottom(i,j)*uCur(i-1,j) + ...
-                            constantLeft(i,j)*uCur(i,j) + constantRight(i,j)*uCur(i,j+1) + ...
+                            constantLeft(i,j)*(2*uCur(i,j)-uCur(i,j+1)) + constantRight(i,j)*uCur(i,j+1) + ...
                             constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
             elseif j == n
                 uFut(i,j) = constantTop(i,j)*uCur(i+1,j) + constantBottom(i,j)*uCur(i-1,j) + ...
-                            constantLeft(i,j)*uCur(i,j-1) + constantRight(i,j)*uCur(i,j) + ...
+                            constantLeft(i,j)*uCur(i,j-1) + constantRight(i,j)*(2*uCur(i,j)-uCur(i,j-1)) + ...
                             constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
             else
                 uFut(i,j) = constantTop(i,j)*uCur(i+1,j) + constantBottom(i,j)*uCur(i-1,j) + ...
                             constantLeft(i,j)*uCur(i,j-1) + constantRight(i,j)*uCur(i,j+1) + ...
                             constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
             end
-            if(abs(uFut(i,j)) > 1)
-                uFut(i,j) = 1;
+            if uFut(i,j) < 0
+                uFut(i,j) = 0;
             end
         end
     end
     
     surfl(x,y,uCur,ss,kk), xlabel('X axis'), ylabel('Y axis'),
-                 title('Wave'), axis([0 4 0 4 -1 2]),
+                 title('Wave'), axis([0 4 0 4 0 1]),
                  shading interp
     drawnow
     uPast = uCur;
