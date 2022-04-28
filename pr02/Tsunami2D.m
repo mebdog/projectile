f769f48150d93fc3812de1fde24e6d9c6731f846
connorhorn26 <connor.horn@colorado.edu>
2022-04-27 18:27:25 -0600

    added constants

--- pr02/Tsunami2D.m
+++ pr02/Tsunami2D.m
@@ -9,18 +9,19 @@ xLeft = 0;
 xRight = 4;
 yLeft = 0;
 yRight = 4;
-y = linspace(yRight, yLeft, 201);
-x = linspace(xRight, xLeft, 201);
+y = linspace(yLeft, yRight, 201);
+x = linspace(xLeft, xRight, 201);
 n = length(x);
 uPast = zeros(n,n);
 uCur = zeros(n,n);
 uFut = zeros(n,n);
 
 [h, hx, hy] = getDepth(x,y);
-constantTop = [];
-constantBottom = [];
-constantLeft = [];
-constantRight = [];
+constantTop = lam*(h+hx*deltaX/2); %u(i+1,j)
+constantBottom = lam*(h-hx*deltaX/2); %u(i-1,j)
+constantLeft = lam*(h-hy*deltaX/2); %u(i,j-1)
+constantRight = lam*(h+hy*deltaX/2);%u(i,j+1)
+constantCurrent = 2-4*lam*h; %u(i,j)
 
 mesh(x,y,uCur);
 drawnow
@@ -30,27 +31,44 @@ for t = 1:nFrames
         for i = 1:n
             if i == 1
                 uFut(i,j) = leftBoundary(t, deltaT);
-            % Corner Cases
-            elseif i == n && j == 1
-                uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i,j) + uCur(i,j) + uCur(i,j+1)) - uPast(i,j);
-            elseif i == n && j == n
-                uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i,j) + uCur(i,j-1) + uCur(i,j)) - uPast(i,j);
-            % Edge Cases
-            elseif i == n
-                uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i,j) + uCur(i,j-1) + uCur(i,j+1)) - uPast(i,j);
-            elseif j == n
-                uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i+1,j) + uCur(i,j-1) + uCur(i,j)) - uPast(i,j);
-            elseif j == 1
-                uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i+1,j) + uCur(i,j) + uCur(i,j+1)) - uPast(i,j);
-            else
-                uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i+1,j) + uCur(i,j-1) + uCur(i,j+1)) - uPast(i,j);
+                continue;
             end
+            if i > 1 && i < n && j > 1 && j < n
+                phantomTop = uCur(i+1,j);
+                phantomLeft = uCur(i,j-1);
+                phantomRight = uCur(i,j+1);
+            else
+                [phantomTop, phantomLeft, phantomRight] = getPhantom(i,j,n,uCur);
+            end
+           
+            uFut(i,j) = constantBottom(i,j)*uCur(i-1,j) + constantTop(i,j)*phantomTop  ...
+                      + constantLeft(i,j)*phantomLeft + constantRight(i,j)*phantomRight ...
+                      + constantCurrent(i,j)*uCur(i,j) - uPast(i,j);
+
+            if i == 2 && j == 1
+                disp([phantomRight, phantomTop, phantomLeft]);
+                disp(uFut(i,j));
+            end
+            % Corner Cases
+            %elseif i == n && j == 1
+            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i,j) + uCur(i,j) + uCur(i,j+1)) - uPast(i,j);
+            %elseif i == n && j == n
+            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i,j) + uCur(i,j-1) + uCur(i,j)) - uPast(i,j);
+            % Edge Cases
+            %elseif i == n
+            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i,j) + uCur(i,j-1) + uCur(i,j+1)) - uPast(i,j);
+            %elseif j == n
+            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i+1,j) + uCur(i,j-1) + uCur(i,j)) - uPast(i,j);
+            %elseif j == 1
+            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i+1,j) + uCur(i,j) + uCur(i,j+1)) - uPast(i,j);
+            %else
+            %    uFut(i,j) = (2-4*lam)*uCur(i,j) + lam*(uCur(i-1,j) + uCur(i+1,j) + uCur(i,j-1) + uCur(i,j+1)) - uPast(i,j);
+            %end
         end
     end
     
     mesh(x,y,uCur),xlabel('X axis'), ylabel('Y axis');
     drawnow
-
     uPast = uCur;
     uCur = uFut;
 end
